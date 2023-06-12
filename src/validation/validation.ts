90fc5e2ce37be18fb35ffd63bcf5ab88b196a209
amirnasr <amir.nasr72@gmail.com>
2023-06-12 18:34:13 +0330

    implemen validation schema for signUp form

---
+++ src/validation/validation.ts
@@ -0,0 +1,9 @@
+import * as YUP from "yup";
+
+export const signUpSchema = YUP.object().shape({
+    firstname:YUP.string().required(),
+    lastname:YUP.string().required(),
+    email:YUP.string().required().email(),
+    password:YUP.string().required(),
+    confirmPass:YUP.string().required().oneOf([YUP.ref("password")])
+});
