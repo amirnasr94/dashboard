7b85f4138f0fbf2406afea2eba1c81c572ea6ba2
amirnasr <amir.nasr72@gmail.com>
2023-06-13 14:58:42 +0330

    add dir of files in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export {default as Container} from "./login&register/Container";
 export {default as SignUp} from "./login&register/SignUp";
+export {default as Login} from "./login&register/Login";
