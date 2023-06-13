2853560294b1060a528e72992a8f9f4b70caa5e1
amirnasr <amir.nasr72@gmail.com>
2023-06-13 15:01:06 +0330

    some edite in route file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import {createBrowserRouter} from "react-router-dom";
 // import MainLayout from "../layout/MainLayout";
-import { SignUp } from "../components";
+import { Container } from "../components";
 
 export const router = createBrowserRouter([
     {
         path:"/",
-        element: <SignUp/>,
+        element: <Container/>,
         // errorElement:(<h3>چیزی پیدا نکردم متاسفانه!</h3>)
     }
 ])
